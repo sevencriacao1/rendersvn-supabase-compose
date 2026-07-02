e7eb99fcb921742d3ddbf446c4153fc566726b7f
sevencriacao1 <criacao1@sevengroup360.com.br>
2026-07-02 16:16:44 -0400

    Add security advisor fixes

---
+++ security-advisor-fixes.sql
@@ -0,0 +1,12 @@
+create or replace function app_private.set_updated_at()
+returns trigger
+language plpgsql
+set search_path = pg_catalog
+as $$
+begin
+  new.updated_at = now();
+  return new;
+end;
+$$;
+
+drop extension if exists pg_graphql;
